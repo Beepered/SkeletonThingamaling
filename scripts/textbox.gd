96f21a4f583301557915dfe7aee729bc64ef1c97
Jacqueline <jacqueline.gracey@gmail.com>
2024-11-02 19:20:08 -0700

    Quotes added (confusion)

--- scripts/textbox.gd
+++ scripts/textbox.gd
@@ -2,19 +2,24 @@ extends Node2D
 
 var quotes = [
 	"I think... I miss my wife.",
-	"You look like the 4th of July,\n makes me want a hot dog real bad.",
+	"You look like the 4th of July,\n makes me want a hot dog\n real bad.",
 	"Throw me,\n BOB THROW ME!",
 	"21:33",
 	"I don't want your love...\nWho said that?!",
-	"Door hinge, cloor finge, If 12 ducks in a row then how many ducks in my glow stick\nPOWER UP!!!",
+	"Door hinge, cloor finge, If 12\n ducks in a row then how many ducks in my glow stick\nPOWER UP!!!",
 	"It's not enough sadness.",
 	"The sun, moons, and stars don't have anything on me.",
-	"Though bleached by sun and\nwind, my presence tells of\ncycles unending, and in solitude,\nI touch the infinite.",
-	"In this sandy symphony of\nstillness, I chew the cud of\n my thoughts",
-	"To live is to suffer, to\nsurvive is to find some\n meaning in the suffering.",
+#	"Though bleached by sun and\nwind, my presence tells of\ncycles unending, and in solitude,\nI touch the infinite.",
+#	"In this sandy symphony of\nstillness, I chew the cud of\n my thoughts",
+#	"To live is to suffer, to\nsurvive is to find some\n meaning in the suffering.",
 	"I wonder what\nchicken tastes like.",
-	"Patience is bitter, but\nits fruit is sweet.",
-	"Yeeeehaw.\nI've always wanted to say that."
+#	"Patience is bitter, but\nits fruit is sweet.",
+	"Yeeeehaw.\nI've always wanted to say that.",
+	"我喜欢冰淇淋",
+	"RAH SIR",
+	"Do it DO it DO\n IT DO IT DO IT",
+	"If you dont know what you're\n doing then just say that. \nYou think I'm going to judge you or something?\n You're pathetic. Scum\n Leave this Earth.",
+	"REE ROO REE ROO REE ROO"
 ]
 
 var current_quote_index = 0
